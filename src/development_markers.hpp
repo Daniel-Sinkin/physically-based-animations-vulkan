c13a8ce4eb51aa99ba2c29d89f39b22b723cea4e
Daniel-Sinkin <danielsinkin97@gmail.com>
2026-05-18 11:55:28 +0200

    Updated to new template

--- src/development_markers.hpp
+++ src/development_markers.hpp
@@ -1,6 +1,41 @@
+// src/development_markers.hpp
+
+#ifndef PHYSICALLY_BASED_ANIMATIONS_VULKAN_SRC_DEVELOPMENT_MARKERS_HPP
+#define PHYSICALLY_BASED_ANIMATIONS_VULKAN_SRC_DEVELOPMENT_MARKERS_HPP
+
 #pragma once
+
+#include <type_traits>
+
+#if defined(mut)
+#    error "mut marker macro is already defined"
+#endif
+
+#if defined(cpy)
+#    error "cpy marker macro is already defined"
+#endif
+
+#if defined(def)
+#    error "def marker macro is already defined"
+#endif
+
+#if defined(__clang__)
+#    define mut [[clang::annotate("mut")]]
+#    define cpy [[clang::annotate("cpy")]]
+#else
+#    define mut
+#    define cpy
+#endif
+
+#define def auto
 
 namespace physically_based_animations_vulkan
 {
-inline constexpr auto k_project_name = "physically-based-animations-vulkan";
+template <typename T>
+[[nodiscard]] constexpr def copy(const T& value) -> std::remove_cvref_t<T>
+{
+    return value;
+}
 }  // namespace physically_based_animations_vulkan
+
+#endif  // PHYSICALLY_BASED_ANIMATIONS_VULKAN_SRC_DEVELOPMENT_MARKERS_HPP
--- src/pch.hpp
+++ src/pch.hpp
@@ -1,5 +1,8 @@
 // src/pch.hpp
 
+#ifndef PHYSICALLY_BASED_ANIMATIONS_VULKAN_SRC_PCH_HPP
+#define PHYSICALLY_BASED_ANIMATIONS_VULKAN_SRC_PCH_HPP
+
 #pragma once
 
 #include <algorithm>      // IWYU pragma: keep
@@ -42,3 +45,5 @@
 #if defined(__ARM_NEON) || defined(__ARM_NEON__)
 #    include <arm_neon.h>  // IWYU pragma: keep
 #endif
+
+#endif  // PHYSICALLY_BASED_ANIMATIONS_VULKAN_SRC_PCH_HPP
